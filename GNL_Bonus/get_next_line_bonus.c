a874e135502031066fff652f51278d0552f1fb9b
Ishta <inabakka@student.42berlin.de>
2025-02-03 19:23:17 +0100

    update

---
+++ GNL_Bonus/get_next_line_bonus.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inabakka <inabakka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 11:38:21 by inabakka          #+#    #+#             */
+/*   Updated: 2025/02/03 17:34:50 by inabakka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	unsigned int	i;
+	char			needle;
+
+	i = 0;
+	needle = (char) c;
+	while (str[i] != '\0')
+	{
+		if (str[i] == needle)
+			return ((char *) &str[i]);
+		i++;
+	}
+	if (str[i] == needle)
+		return ((char *) &str[i]);
+	return (NULL);
+}
+
+static char	*read_from_file(char *basin_buffer, int fd)
+{
+	int		bytes_read;
+	char	*temp_buffer;
+	char	*new_basin_buffer;
+
+	temp_buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!temp_buffer)
+		return (NULL);
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+			return (free(temp_buffer), NULL);
+		temp_buffer[bytes_read] = '\0';
+		new_basin_buffer = ft_strjoin(basin_buffer, temp_buffer);
+		if (!new_basin_buffer)
+			return (free(temp_buffer), NULL);
+		free(basin_buffer);
+		basin_buffer = new_basin_buffer;
+		if (ft_strchr(new_basin_buffer, '\n'))
+			break ;
+	}
+	free(temp_buffer);
+	return (basin_buffer);
+}
+
+char	*get_next_line_bonus(int fd)
+{
+	static char	*basin_buffer[1000];
+	char		*line;
+	char		*newline_pos;
+	char		*remaining_content;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!basin_buffer[fd])
+		basin_buffer[fd] = ft_calloc(BUFFER_SIZE, sizeof(char));
+	if (!ft_strchr(basin_buffer[fd], '\n'))
+		basin_buffer[fd] = read_from_file(basin_buffer[fd], fd);
+	if (!basin_buffer[fd])
+		return (NULL);
+	newline_pos = ft_strchr(basin_buffer[fd], '\n');
+	line = ft_substr(basin_buffer[fd], 0, newline_pos - basin_buffer[fd] + 1);
+	remaining_content = ft_strdup(newline_pos + 1);
+	free(basin_buffer[fd]);
+	basin_buffer[fd] = remaining_content;
+	if (ft_strlen(remaining_content) <= 0)
+	{
+		free(basin_buffer[fd]);
+		return (line);
+	}
+	return (line);
+}
---
+++ GNL_Bonus/main_bonus.c
@@ -0,0 +1,47 @@
+#include "get_next_line_bonus.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd1;
+	int		fd2;
+	char	*line1;
+	char	*line2;
+
+	fd1 = open("text1.txt", O_RDONLY);
+	fd2 = open("text2.txt", O_RDONLY);
+	if (fd1 == -1 || fd2 == -1)
+	{
+		printf("Error opening one or both files, try again.\n");
+		return (1);
+	}
+	// line = get_next_line(fd);
+	// printf("line#%d --- %s",count++, line);
+	// line = get_next_line(fd);
+	// printf("line#%d --- %s",count++, line);
+	// line = get_next_line(fd);
+	// printf("line#%d --- %s",count++, line);
+	// free(line);
+	printf("BUFFER_SIZE: %d\n", BUFFER_SIZE);
+	while(1)
+	{
+		line1 = get_next_line_bonus(fd1);
+		line2 = get_next_line_bonus(fd2);
+		printf("text1: %s\n", line1);
+		printf("text2: %s\n", line2);
+		if(line1 == NULL || line2 == NULL)
+		{
+			free(line1);
+			free(line2);
+			break;
+		}
+		free(line1);
+		free(line2);
+		line1 = NULL;
+		line2 = NULL;
+	}
+	printf("\n");
+	close(fd1);
+	close(fd2);
+	return(0);
+}
